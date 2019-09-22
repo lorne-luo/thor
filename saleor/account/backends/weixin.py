6d5cc939221be6a37a03bf0e7472d6cc95fa354f
Lorne <dev@luotao.net>
2019-09-22 18:43:26 +1000

    test code

--- saleor/account/backends/weixin.py
+++ saleor/account/backends/weixin.py
@@ -1,4 +1,9 @@
+import json
+
+from django.http import HttpResponse
+from requests import HTTPError
 from social_core.backends.weixin import WeixinOAuth2, WeixinOAuth2APP
+from social_core.exceptions import AuthCanceled, AuthUnknownError
 
 from . import BaseBackend
 from ...site import AuthenticationBackends
@@ -12,3 +17,28 @@ class CustomWeixinMpOAuth2(BaseBackend, WeixinOAuth2APP):
     DB_NAME = AuthenticationBackends.WEIXINMP
     name = 'weixinmp'
     DEFAULT_SCOPE = ['snsapi_userinfo']
+
+    def auth_complete(self, *args, **kwargs):
+        """Completes login process, must return user instance"""
+        self.process_error(self.data)
+        try:
+            response = self.request_access_token(
+                self.ACCESS_TOKEN_URL,
+                data=self.auth_complete_params(self.validate_state()),
+                headers=self.auth_headers(),
+                method=self.ACCESS_TOKEN_METHOD
+            )
+        except HTTPError as err:
+            if err.response.status_code == 400:
+                raise AuthCanceled(self)
+            else:
+                raise
+        except KeyError:
+            raise AuthUnknownError(self)
+
+        if 'errcode' in response:
+            return HttpResponse(json.dumps(response))
+            # raise AuthCanceled(self)
+        self.process_error(response)
+        return self.do_auth(response['access_token'], response=response,
+                            *args, **kwargs)
