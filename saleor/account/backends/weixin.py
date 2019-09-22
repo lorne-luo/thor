dc0a9fc8ae94455167a5d2054a22aa6d2e2fd82f
Lorne <dev@luotao.net>
2019-09-22 18:33:59 +1000

    update scope

--- saleor/account/backends/weixin.py
+++ saleor/account/backends/weixin.py
@@ -1,13 +1,14 @@
 from social_core.backends.weixin import WeixinOAuth2, WeixinOAuth2APP
 
-from ...site import AuthenticationBackends
 from . import BaseBackend
+from ...site import AuthenticationBackends
 
 
 class CustomWeixinOAuth2(BaseBackend, WeixinOAuth2):
     DB_NAME = AuthenticationBackends.WEIXIN
 
+
 class CustomWeixinMpOAuth2(BaseBackend, WeixinOAuth2APP):
     DB_NAME = AuthenticationBackends.WEIXINMP
     name = 'weixinmp'
-
+    DEFAULT_SCOPE = ['snsapi_userinfo']
