a18eaddb4b3e8bbe226f24690ddc70c96cd6479f
Lorne <dev@luotao.net>
2020-01-24 17:04:57 +1100

    feat: weixin login from scratch

--- saleor/weixin/urls.py
+++ saleor/weixin/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from django.views.decorators.csrf import csrf_exempt
 
-from .views import weixin
+from .views import weixin, weixin_login, weixin_authorize
 
 urlpatterns = [
+    # url(r"^login/", weixin_login, name='login'),
+    # url(r"^authorize/", weixin_authorize, name='authorize'),
     url(r'^$', csrf_exempt(weixin.django_view_func()), name='index'),
 
     # login entry /login/weixinmp/
--- saleor/weixin/views.py
+++ saleor/weixin/views.py
@@ -1,7 +1,15 @@
 import logging
+from datetime import datetime, timedelta
 
 from django.conf import settings
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 from weixin import Weixin
+
+from saleor.account.models import User
 
 logger = logging.getLogger(__name__)
 
@@ -42,3 +50,71 @@ def subscribe(**kwargs):
     """
     print(kwargs)
     return "欢迎订阅我的公众号"
+
+
+def create_weixin_user(openid, user_info):
+    raise NotImplementedError
+
+
+def authorize_weixin_user(openid, user_info):
+    email = f'{openid}@weixin.auth'
+    try:
+        user = User.objects.get_by_natural_key(email)
+        user.is_active = True
+        user.private_meta = user_info
+        user.save()
+    except User.DoesNotExist:
+        user = create_weixin_user(openid, user_info)
+    return user
+
+
+@csrf_exempt
+def weixin_authorize(request):
+    logger.info(f'##### GET={dict(request.GET)} POST={dict(request.POST)}')
+    code = request.GET.get("code")
+    next = request.GET.get("next") or '/'
+    data = weixin.access_token(code)
+    openid = data.get('openid')
+    access_token = data.get('access_token')
+    from pprint import pprint
+    pprint(data)
+    # {'access_token': '29_2JWyReUY3SWy2FWGHyyxFCAGNajok4eVHkYr5Rr-_b9qkhjhiG5Q-cMdpa6bPpFbKOGDphSVmFOMb8OZwi6nWQ',
+    #  'errcode': {},
+    #  'expires_in': 7200,
+    #  'openid': 'o4vCet0gGGRzWyyQK421Vdt8d67Y',
+    #  'refresh_token': '29_PBZRj5lvk7kIwMRlXoQAdzTL1f4Pfssb4yTLxJeoadvFLqTP-hRUK_hY-hw0JgL6L8acATnziu85H5cmfXGG2w',
+    #  'scope': 'snsapi_userinfo'}
+    user_info = weixin.userinfo(access_token, openid)
+    pprint(user_info)
+    # {'city': '',
+    #  'country': '澳大利亚',
+    #  'errcode': {},
+    #  'headimgurl': 'http://thirdwx.qlogo.cn/mmopen/vi_32/ajNVdqHZLLApDVEnXToNtLC2HxoUMoFdQ4EMsMicva3Ol8K9rFUjv9ianXicx3gm1QFHFFYC3obdft73RviaX4F1vQ/132',
+    #  'language': 'en',
+    #  'nickname': '罗韬',
+    #  'openid': 'o4vCet0gGGRzWyyQK421Vdt8d67Y',
+    #  'privilege': [],
+    #  'province': '维多利亚',
+    #  'sex': 1}
+    user = authorize_weixin_user(openid, user_info)
+    login(request, user)
+
+    expiry = datetime.now() + timedelta(days=1)
+    response = HttpResponseRedirect(next)
+    response.set_cookie('openid', openid)
+    response.set_cookie('expiry', expiry)
+    return response
+
+
+@csrf_exempt
+def weixin_login(request):
+    logger.info(f'##### GET={dict(request.GET)} POST={dict(request.POST)}')
+    openid = request.GET.get("openid")
+    next = request.GET.get("next")
+    if openid and next:
+        return redirect(next)
+
+    callback_url = reverse('weixin:authorize')
+    callback = callback_url
+    url = weixin.authorize(callback, "snsapi_userinfo")
+    return redirect(url)
